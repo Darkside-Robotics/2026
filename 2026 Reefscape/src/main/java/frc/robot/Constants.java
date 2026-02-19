23bb9aaac2884a5fd7d44959008feab29720b60f
Darkside Coding <darksiderobots@gmail.com>
2026-02-19 18:03:44 -0500

    Swerve module update to match pose example interface

--- 2026 Reefscape/src/main/java/frc/robot/Constants.java
+++ 2026 Reefscape/src/main/java/frc/robot/Constants.java
@@ -9,20 +9,7 @@ import edu.wpi.first.math.util.Units;
 
 public final class Constants {
 
-    public static final class ModuleConstants {
-        public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
-        public static final double kDriveMotorGearRatio = 1 / 6.75;
-        public static final double kTurningMotorGearRatio = 1 / 12.8;
-        public static final double kDistancePerWheelRotation = Math.PI * kWheelDiameterMeters;
-        public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * kDistancePerWheelRotation;
-        public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
-        public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
-        public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
-        public static final double kPTurning = 0.5;
-        public static final double kDriveMotorFeedforwardVoltsPerRotationsPerMinute = 1/473;        
-        public static final double kDriveMotorFeedforwardVoltsPerRotationsPerSecond = 1/(473 / 60);        
-        public static final double kDriveMotorFeedforwardVoltsPerMetersPerSecond = 1/(473 / 60 * kDistancePerWheelRotation * kDriveMotorGearRatio);
-    }
+
 
     public static final class LEDConstants 
     {       
