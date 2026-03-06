ab5a2932956140f10cf0a4ed6a48bea44dbe7381
Darkside Robotics <darksiderobots@gmail.com>
2026-03-05 19:32:47 -0500

    lighting update

--- 2026 Rebuilt Lighting/src/main/java/frc/robot/subsystems/LEDSubsystem2.java
+++ 2026 Rebuilt Lighting/src/main/java/frc/robot/subsystems/LEDSubsystem2.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Array;
 import java.security.Key;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
 
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
@@ -27,15 +28,20 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class LEDSubsystem2 extends SubsystemBase {
-  private static final int PWM_PORT = 9;
-  private static final int NUMBER_OF_LEDS = 100;
+  private static final int PWM_PORT = 9;            // Port From ROBORIO
+  
+  private static final int WIDTH_LEDNUM = 42; // 71/(5/3)
+  private static final int LENGTH_LEDNUM = 45;// 76/(5/3)
+    private static final int NUMBER_OF_LEDS = WIDTH_LEDNUM*2 + LENGTH_LEDNUM*2;// Yea
 
   private static Color targetingColor = Color.kWhite;
   private boolean targetingBlink = false;
+  private boolean targetingRacer = true;
 
   private final AddressableLED led;
   private final AddressableLEDBuffer ledBuffer;
- 
+ private final AddressableLEDBufferView[] ledSides = new AddressableLEDBufferView[4];
+  
   // private final AddressableLEDBufferView buff;
   //Green and Red are flipped on the strip. The colors are swapped on purpose;
   private final Color RedColor = new Color(0, 255, 0);
@@ -47,7 +53,6 @@ public class LEDSubsystem2 extends SubsystemBase {
 
     led = new AddressableLED(PWM_PORT);
     led.setLength(NUMBER_OF_LEDS);
-
     ledBuffer = new AddressableLEDBuffer(NUMBER_OF_LEDS);
 
     led.setData(ledBuffer);
@@ -58,30 +63,43 @@ public class LEDSubsystem2 extends SubsystemBase {
     // the last command to run will continue to be displayed.
     // Note: Other default patterns could be used instead!
     //setDefaultCommand(UpdatePatternCommand().withName("Off"));
+    int lastLED = 0;
+
+    for (int i = 0; i < 2; i++) {
+      ledSides[i] = ledBuffer.createView(lastLED, lastLED + WIDTH_LEDNUM -1);
+      lastLED += WIDTH_LEDNUM;
+    }
+    for (int i = 0; i < 2; i++) {
+      ledSides[i] = ledBuffer.createView(lastLED, lastLED + LENGTH_LEDNUM-1);
+      lastLED += LENGTH_LEDNUM;
+    }
   }
-  
+
 
   // PUBLIC FUNCTIONS
   public void targetSearch() {
-    targetingColor = (YellowColor);
+    //targetingColor = (YellowColor);
     targetingBlink = false;
-
+    targetingRacer = true;
   }
 
   public void targetFound() {
     targetingColor = (YellowColor);
     targetingBlink = true;
+    targetingRacer = false;
   }
  
  
     public void targetLocked() {
-    targetingColor = (RedColor);
+    targetingColor = (Color.kBlack);
     targetingBlink = false;
+    targetingRacer = false;
   }
 
   public void firingTurret() {
   targetingColor = (purpleColor);
   targetingBlink = false;
+  targetingRacer = false;
   }
 
 
@@ -94,18 +112,23 @@ public class LEDSubsystem2 extends SubsystemBase {
     led.setData(ledBuffer);
   }
 // Any change of LED pattern should happen in function below
+LEDPattern ledBase = LEDPattern.rainbow(255, 255); 
+      Map<Double, Color> maskSteps = Map.of(0.0, Color.kWhite, 0.5, Color.kBlack);
+      LEDPattern mask = LEDPattern.steps(maskSteps).scrollAtRelativeSpeed(Percent.per(Second).of(20));
+      LEDPattern Racerpattern = ledBase.mask(mask);
+
    public void updatePattern() {
+    
+    if (targetingRacer) {Racerpattern.applyTo(ledBuffer); return;}
+    
     SmartDashboard.putString("Pattern", targetingColor.toString());
-   
       LEDPattern ledBase = LEDPattern.solid(targetingColor);
     if(targetingBlink){
       ledBase = ledBase.blink(Seconds.of(0.25), Seconds.of(0.25));
     }
     ledBase.applyTo(ledBuffer);
-  
+    LEDPattern.solid(Color.kBlack).applyTo(ledSides[1]);
   }
-
-
     /**
    * Creates a command that runs a pattern on the entire LED strip.
    *
